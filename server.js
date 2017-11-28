916e855d0da972f1417845550f5204b7ef58011e
Nik Bornemeier <NikolausRB@gmail.com>
2017-11-28 08:37:07 -0700

    add additional endpoints for server

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ app.locals.title = 'Palette Picker';
 app.get('/', (request, response) => {
 });
 
+app.get('/projects', (request, response) => {
+  response.send('projects placeholder!');
+});
+
+app.get('/projects/:id', (request, response) => {
+  response.send('projects ID placeholder!');
+});
+
+// app.get('/projects/:id/palettes' (request, response) => {
+//   response.send('project palettes placeholder');
+// });
+
+// app.get('/projects/:id/palettes/:id', (request, response) => {
+//   response.send('project palettes ID placeholder');
+// });
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
